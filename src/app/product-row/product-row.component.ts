bc4c62fc789ad8cf561146c33b3e050cbb9af3a1
Abir Mahmud <amabirmahmud@gmail.com>
2021-04-30 00:09:14 +0600

    functionalities of product-row added

--- src/app/product-row/product-row.component.ts
+++ src/app/product-row/product-row.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Product} from '../product.model';
 
 @Component({
   selector: 'product-row',
   templateUrl: './product-row.component.html'
 })
 export class ProductRowComponent implements OnInit {
+  @Input() product?: Product;
+  @HostBinding('attr.class') cssClass = 'item';
+
 
   constructor() { }
 
