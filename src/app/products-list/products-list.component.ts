a2028778797f6b6675bce9f47427e4cf9b54b622
Abir Mahmud <amabirmahmud@gmail.com>
2021-04-29 23:58:50 +0600

    functionalities of products-list added

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Product} from '../product.model';
 
 @Component({
-  selector: 'app-products-list',
+  selector: 'products-list',
   templateUrl: './products-list.component.html'
 })
 export class ProductsListComponent implements OnInit {
+  @Input() productList: Product[] = [];
 
-  constructor() { }
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onProductSelected: EventEmitter<Product>;
+
+  private currentProduct?: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
+
 
   ngOnInit(): void {
   }
