a682243d9e709bdc3fe7ea946b6a77f83a9ce80a
Abir Mahmud <amabirmahmud@gmail.com>
2021-04-30 00:06:44 +0600

    product-row component added

---
+++ src/app/product-row/product-row.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'product-row',
+  templateUrl: './product-row.component.html'
+})
+export class ProductRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
