b63c95014821c3701031497cb80b0debf067260f
Abir Mahmud <amabirmahmud@gmail.com>
2021-04-30 00:52:28 +0600

    Added image viewing feature

--- src/app/product-image/product-image.component.ts
+++ src/app/product-image/product-image.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Product} from '../product.model';
 
 @Component({
-  selector: 'app-product-image',
-  template: ``,
+  selector: 'product-image',
+  template: `
+    <img class="product-image" [src]="product?.imageUrl" alt="">
+  `,
 })
 export class ProductImageComponent implements OnInit {
+  @Input() product?: Product;
+  @HostBinding('attr.class') cssClass = 'ui small image';
 
   constructor() { }
 
