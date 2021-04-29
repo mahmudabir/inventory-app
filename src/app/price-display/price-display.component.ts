3fecf21aca375ae8d0b2ed059ae3f7876bef9f6c
Abir Mahmud <amabirmahmud@gmail.com>
2021-04-30 01:40:34 +0600

    Added price viewing feature

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-price-display',
-  template: ``,
+  selector: 'price-display',
+  template: `
+    <div class="price-display">\\$ {{price}}</div>
+  `,
 })
 export class PriceDisplayComponent implements OnInit {
+  @Input() price?: number;
 
   constructor() { }
 
